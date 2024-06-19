88f4796a3451c308c5d3e90c152d9b2c0019ea54
Kryzp0 <chris.jeffry.vega@gmail.com>
2024-06-19 17:49:36 -0400

    Se añade lista con rutas protegidas para usar un map y ahorrar codigo en el HOC

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import HeaderMain from "./layouts/HeaderMain"
-import Cards from "./pages/Cards"
-import Accounts from "./pages/Accounts"
-import Loans from "./pages/Loans"
-import ApplyLoan from "./pages/ApplyLoan"
-import ApplyCard from "./pages/ApplyCard"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import LandingPage from "./pages/LandingPage"
-import { useState } from 'react'
-import PrivateRouteHOC from "./HOCs/PrivateRouteHOC"
-import PublicRoute from "./components/PublicRoute"
-import ApplyTransaction from "./pages/ApplyTransaction"
-import AccountDetails from "./pages/AccountDetails"
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HeaderMain from "./layouts/HeaderMain";
+import LandingPage from "./pages/LandingPage";
+import PublicRoute from "./components/PublicRoute";
+import PrivateRouteHOC from "./HOCs/PrivateRouteHOC";
 
+import Accounts from "./pages/Accounts";
+import Cards from "./pages/Cards";
+import Loans from "./pages/Loans";
+import ApplyLoan from "./pages/ApplyLoan";
+import ApplyCard from "./pages/ApplyCard";
+import ApplyTransaction from "./pages/ApplyTransaction";
+import AccountDetails from "./pages/AccountDetails";
+
+const protectedRoutes = [
+  { path: "/accounts", component: Accounts },
+  { path: "/cards", component: Cards },
+  { path: "/loans", component: Loans },
+  { path: "/apply-loan", component: ApplyLoan },
+  { path: "/apply-card", component: ApplyCard },
+  { path: "/apply-transaction", component: ApplyTransaction },
+  { path: "/transactions", component: ApplyTransaction },
+  { path: "/account-details/:id", component: AccountDetails }
+];
 
 function App() {
   const [showLogin, setShowLogin] = useState(true);
@@ -33,25 +44,22 @@ function App() {
     <BrowserRouter>
       <HeaderMain setLoginOrRegister={setLoginOrRegister} headerOpen={headerOpen} toggleHeader={toggleHeader} showLogin={showLogin} toggleLoginOrRegister={toggleLoginOrRegister}>
         <Routes>
-        <Route path="/accounts" element={<PrivateRouteHOC><Accounts /></PrivateRouteHOC>} />
-          <Route path="/cards" element={<PrivateRouteHOC><Cards /></PrivateRouteHOC>} />
-          <Route path="/loans" element={<PrivateRouteHOC><Loans /></PrivateRouteHOC>} />
-          <Route path="/apply-loan" element={<PrivateRouteHOC><ApplyLoan /></PrivateRouteHOC>} />
-          <Route path="/apply-card" element={<PrivateRouteHOC><ApplyCard /></PrivateRouteHOC>} />
-          <Route path="/apply-transaction" element={<PrivateRouteHOC><ApplyTransaction /></PrivateRouteHOC>} />
-          <Route path="/transactions" element={<PrivateRouteHOC><ApplyTransaction /></PrivateRouteHOC>} />
-          <Route path="/account-details/:id" element={<PrivateRouteHOC><AccountDetails /></PrivateRouteHOC>} />
+          {protectedRoutes.map(({ path, component: Component }) => {
+            const WrappedComponent = PrivateRouteHOC(Component);
+            return <Route key={path} path={path} element={<WrappedComponent />} />;
+          })}
           <Route
             path="/"
             element={
               <PublicRoute>
                 <LandingPage headerOpen={headerOpen} toggleHeader={toggleHeader} setLoginOrRegister={setLoginOrRegister} />
               </PublicRoute>
-            } />
+            } 
+          />
         </Routes>
       </HeaderMain>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/HOCs/PrivateRouteHOC.jsx
+++ src/HOCs/PrivateRouteHOC.jsx
@@ -3,17 +3,19 @@ import { Navigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { setLoggedIn } from '../redux/actions/loginActions';
 
-const PrivateRouteHOC = (WrappedComponent) => (props) => {
+const PrivateRouteHOC = (WrappedComponent) => {
+  return (props) => {
     const dispatch = useDispatch();
     const state = useSelector(store => store.loginReducer);
     const { loggedIn } = state;
 
     useEffect(() => {
-        const loggedInStatus = localStorage.getItem('loggedIn') === 'true';
-        dispatch(setLoggedIn(loggedInStatus));
+      const loggedInStatus = localStorage.getItem('loggedIn') === 'true';
+      dispatch(setLoggedIn(loggedInStatus));
     }, [dispatch]);
 
     return loggedIn ? <WrappedComponent {...props} /> : <Navigate to="/" />;
+  };
 };
 
 export default PrivateRouteHOC;
